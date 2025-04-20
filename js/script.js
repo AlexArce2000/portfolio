d6826f205b3a5caa0b99d2be59159ab366008cf2
AlexArce2000 <alexisroarce2000@gmail.com>
2025-04-20 01:47:38 -0300

    agregado encabezado + form contactos

--- js/script.js
+++ js/script.js
@@ -9,8 +9,40 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 var typed = new Typed("#typed", {
-    strings: ["Alexis Arce", "Desarrollador Web", "Backend"],
+    strings: ["Desarrollador Web", "Backend"],
     typeSpeed: 100,
     backSpeed: 40,
     loop: true
+});    
+
+var form = document.getElementById("my-form");
+            
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Mensaje envíado exitosamente!";
+      form.reset();
+      location.reload();
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
   });
+}
+form.addEventListener("submit", handleSubmit)
